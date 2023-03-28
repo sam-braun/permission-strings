ff55bd4d671a74c9beec550a1b01aeb7d5312741
Uri Korin <umk2102@columbia.edu>
2023-03-28 17:18:04 -0400

    updated pfind

--- part1/src/pfind.c
+++ part1/src/pfind.c
@@ -41,21 +41,84 @@ int perm_check(char* perm_string) {
 }
 
 char* permission_string(struct stat *statbuf) {
-    char *perm_string;
+    char* perm_string;
     if ((perm_string = malloc(11 * sizeof(char))) == NULL) {
         fprintf(stderr, "Error: malloc failed. %s.\n",
                 strerror(errno));
         exit(EXIT_FAILURE);
     }
-    perm_string[0] = '-';
     for (int i = 0; i < 9; i += 3) {
         // Using the ternary operator for succinct code.
-        perm_string[i + 1] = statbuf->st_mode & perms[i] ? 'r' : '-';
-        perm_string[i + 2] = statbuf->st_mode & perms[i + 1] ? 'w' : '-';
-        perm_string[i + 3] = statbuf->st_mode & perms[i + 2] ? 'x' : '-';
+        perm_string[i] = statbuf->st_mode & perms[i] ? 'r' : '-';
+        perm_string[i + 1] = statbuf->st_mode & perms[i + 1] ? 'w' : '-';
+        perm_string[i + 2] = statbuf->st_mode & perms[i + 2] ? 'x' : '-';
     }
-    perm_string[10] = '\0';
+    perm_string[9] = '\0';
     return perm_string;
+}
+
+void recursive_print(char *name, char *perm_string) {
+	// Recurse through directory	
+	char path[PATH_MAX + 1];
+    if (realpath(name, path) == NULL) {
+        fprintf(stderr, "Error: Cannot get full path of file '%s'. %s.\n",
+                name, strerror(errno));
+        exit(1);
+    }
+
+    DIR *dir;
+    if ((dir = opendir(path)) == NULL) {
+        fprintf(stderr, "Error: Cannot open directory '%s'. %s.\n",
+                path, strerror(errno));
+        exit(1);
+    }
+
+    size_t pathlen = strlen(path);
+    // Only the root folder '/' ends with a '/'.
+    // All paths typically end in the name of the file, as in /home/user.
+    if (strcmp(path, "/")) {
+        // If we don't have the root folder, append a '/'.
+        path[pathlen++] = '/';
+        path[pathlen] = '\0';
+    }
+
+    struct dirent *entry;
+    struct stat sb;
+    while ((entry = readdir(dir)) != NULL) {
+        // Skip . and ..
+        if (strcmp(entry->d_name, ".") == 0 ||
+            strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+        // Add the current entry's name to the end of full_filename, following
+        // the trailing '/' and overwriting the '\0'.
+        strncpy(path + pathlen, entry->d_name, PATH_MAX - pathlen);
+        if (lstat(path, &sb) < 0) {
+            fprintf(stderr, "Error: Cannot stat file '%s'. %s.\n",
+                    path, strerror(errno));
+            continue;
+        }
+        /*
+	// Differentiate directories from other file types.
+        if (S_ISDIR(sb.st_mode)) {
+            printf("%s [directory]\n", path);
+        } else {
+            printf("%s\n", path);
+        }
+	*/
+	
+	fprintf(stdout, "Iterated perm string: %s\n", permission_string(&sb));
+
+	if (strcmp(perm_string, permission_string(&sb)) == 0) {
+		fprintf(stdout, "%s\n", path);
+	}
+
+	if (S_ISDIR(sb.st_mode)) {
+		recursive_print(path, perm_string);
+	}
+	}
+	closedir(dir);
+
 }
 
 int main(int argc, char **argv) {
@@ -109,7 +172,7 @@ int main(int argc, char **argv) {
 
 	// Stat file
 	struct stat buf;
-	if (lstat(dir_name, &buf) < 0) {
+	if (stat(dir_name, &buf) < 0) {
 		fprintf(stderr, "Error: Cannot stat '%s'. %s.\n", dir_name, strerror(errno));
 		return EXIT_FAILURE;
 	}
@@ -118,35 +181,8 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 
-	// Now recurse through given directory
-	DIR *dir;
-	if ((dir = opendir(dir_name)) == NULL) {
-		fprintf(stderr, "Error: Cannot open directory '%s'. %s.\n", dir_name, strerror(errno));
-		return EXIT_FAILURE;
-	}
+	recursive_print(argv[2], perm_string);
 
-	char full_filename[PATH_MAX + 1];
-	size_t pathlen = strlen(full_filename) + 1;
-	full_filename[pathlen - 1] = '/';
-	full_filename[pathlen] = '\0';
-	//strcpy(full_filename, dir_name);
-
-	struct dirent *entry;
-	struct stat sb;
-	while ((entry = readdir(dir)) != NULL) {
-		strncpy(full_filename + pathlen, entry->d_name, PATH_MAX - pathlen);
-		if (lstat(full_filename, &sb) < 0) {
-		fprintf(stderr, "Error: Cannot stat file '%s'. %s.\n",
-			full_filename, strerror(errno));
-			continue;
-		}
-		if (strcmp(permission_string(&sb), perm_string) == 0) {
-			strcat(full_filename, entry->d_name);
-			fprintf(stdout, "%s\n", full_filename);
-		}
-	}
-
-	closedir(dir);
 	return EXIT_SUCCESS;
 
 }
