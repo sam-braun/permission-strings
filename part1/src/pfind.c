f5c858f107bd7a4ca27ec1b737140e48dfac15ec
Samuel Braun <slb2250@columbia.edu>
2023-03-28 16:14:08 -0400

    Uri's copy

--- part1/src/pfind.c
+++ part1/src/pfind.c
@@ -6,62 +6,56 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <limits.h>
+
+int perms[] = {S_IRUSR, S_IWUSR, S_IXUSR,
+               S_IRGRP, S_IWGRP, S_IXGRP,
+               S_IROTH, S_IWOTH, S_IXOTH};
 
 int perm_check(char* perm_string) {
 	if (strlen(perm_string) != 9) {
 		fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-		return EXIT_FAILURE;
+		return -1;
 	}
 
 	for (int i = 0; i < 9; i++) {
 		int j = i % 3;
 		char s = perm_string[i];
-		if (j == 0 && (s != 'r' || s != '-')) {
+		if (j == 0 && !(s == 'r' || s == '-')) {
 				fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-                		return EXIT_FAILURE;		
+                		return -1;		
 		}
 		
-		else if (j == 1 && (s != 'w' || s != '-')) {
+		else if (j == 1 && !(s == 'w' || s == '-')) {
                                 fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-                                return EXIT_FAILURE;
+                                return -1;
                 } 
 
-		else if (j == 2 && (s != 'x' || s != '-')) {
+		else if (j == 2 && !(s == 'x' || s == '-')) {
                                 fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-                                return EXIT_FAILURE;
+                                return -1;
                 }
-	}
-	return 0;
-}
-
-int perm_match(char* legit, char* curr) {
-	for (int i = 0; i < 9; i++) {
-		if (*legit != *curr) {
-			return -1;
-		}
-		legit++;
-		curr++;
 	}
 	return 1;
 }
 
-char* get_perm_string(struct stat srcc) {
-	char[9] ps = "---------";
-	
-	(srcc.stmode & S_IRUSR) ? ps[0] = 'r' : ps[0] = '-';
-	(srcc.stmode & S_IWUSR) ? ps[0] = 'w' : ps[0] = '-';
-	(srcc.stmode & S_IXUSR) ? ps[0] = 'x' : ps[0] = '-';
-
-	(srcc.stmode & S_IRGRP) ? ps[0] = 'r' : ps[0] = '-';
-	(srcc.stmode & S_IWGRP) ? ps[0] = 'w' : ps[0] = '-';
-	(srcc.stmode & S_IXGRP) ? ps[0] = 'x' : ps[0] = '-';
-
-	(srcc.stmode & S_IROTH) ? ps[0] = 'r' : ps[0] = '-';
-	(srcc.stmode & S_IWOTH) ? ps[0] = 'w' : ps[0] = '-';
-	(srcc.stmode & S_IXOTH) ? ps[0] = 'x' : ps[0] = '-';
-
-	return ps;
-
+char* permission_string(struct stat *statbuf) {
+    char *perm_string;
+    if ((perm_string = malloc(11 * sizeof(char))) == NULL) {
+        fprintf(stderr, "Error: malloc failed. %s.\n",
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    perm_string[0] = '-';
+    for (int i = 0; i < 9; i += 3) {
+        // Using the ternary operator for succinct code.
+        perm_string[i + 1] = statbuf->st_mode & perms[i] ? 'r' : '-';
+        perm_string[i + 2] = statbuf->st_mode & perms[i + 1] ? 'w' : '-';
+        perm_string[i + 3] = statbuf->st_mode & perms[i + 2] ? 'x' : '-';
+    }
+    perm_string[10] = '\0';
+    return perm_string;
 }
 
 int main(int argc, char **argv) {
@@ -71,7 +65,7 @@ int main(int argc, char **argv) {
 	opterr = 0;
 	int c;
 	char* dir_name = NULL;
-	char* permission_string = NULL;
+	char* perm_string = NULL;
 
 	// Getopt - options -d, -p, -h
 	while ((c = getopt(argc, argv, "d:p:h")) != -1) {
@@ -86,14 +80,14 @@ int main(int argc, char **argv) {
 			case 'p':
 				fprintf(stdout, "reached case 'p'\n");
 				pflag = 1;
-				permission_string =  optarg;
+				perm_string =  optarg;
 				break;
 			case 'h':
 				fprintf(stdout, "enters case h\n");
 				fprintf(stdout, "Usage: ./pfind -d <directory> -p <permissions string> [-h]\n");
 				return EXIT_SUCCESS;
 			case '?':
-				fprintf(stderr, "Error: Unknown option '%c' received.\n", optopt);
+				fprintf(stderr, "Error: Unknown option '-%c' received.\n", optopt);
 				return EXIT_FAILURE;
 			default:
 				fprintf(stdout, "reaches default case\n");
@@ -112,14 +106,47 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Error: Required argument -d <directory> not found.\n");
 		return EXIT_FAILURE;
 	} 
-	
+
 	// Stat file
 	struct stat buf;
 	if (lstat(dir_name, &buf) < 0) {
 		fprintf(stderr, "Error: Cannot stat '%s'. %s.\n", dir_name, strerror(errno));
 		return EXIT_FAILURE;
 	}
+	
+	if (perm_check(perm_string) == -1) {
+		return EXIT_FAILURE;
+	}
 
-	perm_check(permission_string);
+	// Now recurse through given directory
+	DIR *dir;
+	if ((dir = opendir(dir_name)) == NULL) {
+		fprintf(stderr, "Error: Cannot open directory '%s'. %s.\n", dir_name, strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	char full_filename[PATH_MAX + 1];
+	size_t pathlen = strlen(full_filename) + 1;
+	full_filename[pathlen - 1] = '/';
+	full_filename[pathlen] = '\0';
+	//strcpy(full_filename, dir_name);
+
+	struct dirent *entry;
+	struct stat sb;
+	while ((entry = readdir(dir)) != NULL) {
+		strncpy(full_filename + pathlen, entry->d_name, PATH_MAX - pathlen);
+		if (lstat(full_filename, &sb) < 0) {
+		fprintf(stderr, "Error: Cannot stat file '%s'. %s.\n",
+			full_filename, strerror(errno));
+			continue;
+		}
+		if (strcmp(permission_string(&sb), perm_string) == 0) {
+			strcat(full_filename, entry->d_name);
+			fprintf(stdout, "%s\n", full_filename);
+		}
+	}
+
+	closedir(dir);
+	return EXIT_SUCCESS;
 
 }
