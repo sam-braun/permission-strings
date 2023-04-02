114c877d3c64124f1aab63f07b1ef649a821d9aa
Samuel Braun <slb2250@columbia.edu>
2023-04-02 00:54:33 -0400

    I think it's done?

--- part2/src/spfind.c
+++ part2/src/spfind.c
@@ -1,5 +1,4 @@
 // file for Part 2
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,46 +6,15 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-
 #define MAX_STRLEN	64
 #define MAX_ELEMENTS	1024
 
-int perm_check(char* perm_string) {
-	if (strlen(perm_string) != 9) {
-		fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-		return -1;
-	}
-
-	for (int i = 0; i < 9; i++) {
-		int j = i % 3;
-		char s = perm_string[i];
-		if (j == 0 && !(s == 'r' || s == '-')) {
-				fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-                		return -1;		
-		}
-		
-		else if (j == 1 && !(s == 'w' || s == '-')) {
-                                fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-                                return -1;
-                } 
-
-		else if (j == 2 && !(s == 'x' || s == '-')) {
-                                fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", perm_string);
-                                return -1;
-                }
-	}
-	return 1;
-}
-
 int main(int argc, char *argv[]) {
 
-	// Done testing
 	if (argc == 1) {
 		fprintf(stdout, "Usage: ./spfind -d <directory> -p <permissions string> [-h]\n");
 		return EXIT_SUCCESS;
 	}
-
 	// Set up pipes before forking
 	int pfind_to_sort[2], sort_to_parent[2];
 	if (pipe(pfind_to_sort) == -1) {
@@ -56,32 +24,24 @@ int main(int argc, char *argv[]) {
 	if (pipe(sort_to_parent) == -1) {
 		fprintf(stderr, "Error: pipe failed. %s.\n", strerror(errno));
 		return EXIT_FAILURE;
-
 	}
-	printf("passed piping\n");
-
+	
 	pid_t pid[2];
 	if ((pid[0] = fork()) == -1) {
 		// error forking
 		fprintf(stderr, "Error: fork failed. %s.\n", strerror(errno));
 		return EXIT_FAILURE;
-
 	} else if (pid[0] == 0) {
-		printf("passed first fork - in the child\n");
 		// in pfind process
 		close(pfind_to_sort[0]);
 		dup2(pfind_to_sort[1], STDOUT_FILENO);
 		close(pfind_to_sort[1]);
-
 		// close unrelated processes
 		close(sort_to_parent[0]);
 		close(sort_to_parent[1]);
-
-		fprintf(stderr, "above first execvp\n");
-
 		if (execvp("./pfind", argv) == -1) {      // formerly two "pfind"
 			fprintf(stderr, "Error: pfind failed.\n");
-			exit(EXIT_FAILURE);
+			return EXIT_FAILURE;
 		}
 	}
 	if ((pid[1] = fork()) == -1) {
@@ -89,25 +49,19 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Error: fork failed. %s.\n", strerror(errno));
 		return EXIT_FAILURE;
 	} else if (pid[1] == 0) {
-		fprintf(stderr, "enters sort child\n");
 		// in sort process
 		close(pfind_to_sort[1]);
 		dup2(pfind_to_sort[0], STDIN_FILENO);
 		close(pfind_to_sort[0]);
-
 		close(sort_to_parent[0]);
 		dup2(sort_to_parent[1], STDOUT_FILENO);
 		close(sort_to_parent[1]);
 		
-		fprintf(stderr, "before execlp of sort\n");
-
 		if (execlp("sort", "sort", NULL) == -1) {	// "-o", "stdout"
 			fprintf(stderr, "Error: sort failed.");
 			return EXIT_FAILURE;
 		}
 	}
-
-
 	// in parent now
 	close(pfind_to_sort[0]);
 	close(pfind_to_sort[1]);
@@ -115,12 +69,6 @@ int main(int argc, char *argv[]) {
 	dup2(sort_to_parent[0], STDIN_FILENO);
 	close(sort_to_parent[0]);
 	
-	// in parent
-
-	fprintf(stderr, "enters parent\n");
-	
-	// need to exit failure in case any issue with spfind itself
-
 	pid_t wpid1, wpid2;
 	int status;
 	
@@ -133,25 +81,24 @@ int main(int argc, char *argv[]) {
 	if (wpid2 == -1 || !WIFEXITED(status) || WEXITSTATUS(status) != EXIT_SUCCESS) {
 		return EXIT_FAILURE;
 	}
-
+	
+	
+	
 	// Print directories from pipe
-	char buf[10];
-	int count = 0;
-	while (read(STDIN_FILENO, &buf, sizeof(buf) - 1) != 0) {
+ 	char buf[10];
+ 	int count = 0;
+ 	while (read(STDIN_FILENO, &buf, sizeof(buf) - 1) != 0) {
 		printf("%s", buf);
-		if (strchr(buf, '\n') != NULL) {
-			count++;
-		}
-	}
-	printf("\n");
+ 		if (strchr(buf, '\n') != NULL) {
+ 			count++;
+ 		}
+ 	}
+ 	printf("\n");
 
-	wait(NULL);
-	wait(NULL);
+ 	printf("Total matches: %d\n", count);
 
+ 	wait(NULL);
+ 	wait(NULL);
 	return EXIT_SUCCESS;
 }
-
-
-
-
 
